d66207696d51348c655de3371626ac07730b3daf
Rohit Prakash <rohitp934@gmail.com>
2024-01-08 00:42:36 +0530

    fix for gin

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -95,18 +95,18 @@ func GorillaMux() auth.Middleware {
 	return HttpMiddleware()
 }
 
-type GinContext struct {
-	Request *http.Request
-	Header  func(key, value string)
+type GinContext interface {
+	Header(key, value string)
 }
 
 func Gin() auth.Middleware {
 	return func(context auth.MiddlewareContext) auth.MiddlewareRequestContext {
-		c := context.Args[0].(GinContext)
-		h := createHeadersFromObject(c.Request.Header)
+		r := context.Args[0].(*http.Request)
+		c := context.Args[1].(GinContext)
+		h := createHeadersFromObject(r.Header)
 		return auth.MiddlewareRequestContext{
 			Request: auth.MiddlewareRequest{
-				Method:  c.Request.Method,
+				Method:  r.Method,
 				Headers: h,
 			},
 			SetCookie: func(cookie auth.Cookie) {
